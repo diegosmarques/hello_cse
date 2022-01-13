78c7f0351fae9b496dac35ba329921c3a2016136
diego <diegosmarques@gmail.com>
2022-01-13 12:24:20 +0100

    creating the project

---
+++ database/migrations/2022_01_13_103354_create_celebrities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCelebritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('celebrities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('surnmane');
+            $table->text('description');
+            $table->string('image_path');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('celebrities');
+    }
+}
